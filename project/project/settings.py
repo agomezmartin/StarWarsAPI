975f0ee96550450a3983ae70a1fdeb65dae2eb8a
agomezmartin <agomezmartinpro@gmail.com>
2023-07-05 10:46:43 +0200

    Initial commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -1,5 +1,3 @@
-import os
-
 """
 Django settings for project project.
 
@@ -22,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-=qv56efs&13ue35!bnr+y+c0q9%pm%^c!tyjtyjvg%hf+&#331'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
